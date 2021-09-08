c485aa1e40e80113aa156a66868117536a1f2dc3
Benjamin <Benjamin.Pettit@digimulti.com>
2021-09-09 09:51:10 +1000

    hot reload demo

--- Program.cs
+++ Program.cs
@@ -26,8 +26,40 @@ internal static class HotReloadManager
         //@@bp
         Observable
             .Range(start: 0, count: 20)
-            .Aggregate((x, y) => x+y)
+            .Scan((x, y) => x+y)
             .Subscribe(
                 onNext: Console.WriteLine);
+
+        
+        Observable
+            .Defer(
+                () =>
+                {
+                    var meh = new Meh
+                    {
+                        BoilerPlateProperty = "meh"
+                    };
+                    return Observable.Return(meh);
+                })
+            .Do(Console.WriteLine)
+            .Select(
+                meh =>
+                {
+                    var meh2 = meh with { BoilerPlateProperty2 = "new value" };
+                    return (meh, meh2);
+                })
+            .Do(t => Console.WriteLine(t))
+            .Subscribe();
+    }
+
+    record Meh
+    {
+        public string BoilerPlateProperty { init; get; }
+        public string BoilerPlateProperty2 { init; get; }
+
+        public override string ToString()
+        {
+            return $"{BoilerPlateProperty}, {BoilerPlateProperty2}";
+        }
     }
 }
